015180164848da45daf4dbcf07cda5e940a8d82b
Avinash174 <avinashmagar15@gmail.com>
2023-12-15 14:05:14 +0530

    new changes done

--- lib/repository/post_repo.dart
+++ lib/repository/post_repo.dart
@@ -2,22 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:api_practice/model/post_model.dart';
-import 'package:api_practice/model/product_model.dart';
 import 'package:http/http.dart' as http;
-
-class PoroductRepo {
-  Future<ProductModel> fetchPostApi() async {
-    String Url = 'https://jsonplaceholder.org/posts';
-    final respose = await http.get(Uri.parse(Url));
-
-    if (respose.statusCode == 200) {
-      final body = jsonDecode(respose.body);
-      // log(body.toString());
-      return ProductModel.fromJson(body);
-    }
-    throw Exception('Error');
-  }
-}
 
 class PostRepo {
   Future<PostModel> fetchPostApi() async {
--- lib/view_model/post_view_model.dart
+++ lib/view_model/post_view_model.dart
@@ -3,16 +3,6 @@ import 'dart:developer';
 import 'package:api_practice/model/post_model.dart';
 import 'package:api_practice/repository/post_repo.dart';
 
-// class ProductViewModel {
-//   final _repo = ProductViewModel();
-
-//   Future<ProductModel> fetchProductAPi() async {
-//     final response = await _repo.fetchProductAPi();
-//     log(response.toString());
-//     return response;
-//   }
-// }
-
 class PostViewModel {
   final _repo = PostRepo();
   Future<PostModel> fetchPostApi() async {
